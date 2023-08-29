03cc10c5af036267294cd126c968888aeac3a76f
Lais Frigerio <lais.frigerio@nubank.com.br>
2023-08-29 01:53:01 -0300

    refactoring home page and adding unit tests for FeaturedPost component

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,12 @@
-import Image from 'next/image'
-import { Link } from '../src/components/Link'
+import { FeaturedPost } from '../src/components/FeaturedPost'
 import { PageTitle } from '../src/components/PageTitle'
 import { Header } from '../src/ui/Header'
+
 import { 
   HomeWrapper, 
   FeaturedPosts, 
-  FeaturedPostList, 
-  FeaturedPost, 
-  FeaturedPostDate,
-  FeaturedPostImage,
-  FeaturedPostHeader,
-  FeaturedPostTitle,
-  FeaturedPostSubTitle  } from '../src/components/Home'
+  FeaturedPostList
+} from '../src/components/Home'
 
 export default function HomePage() {
   return (
@@ -21,95 +16,50 @@ export default function HomePage() {
       <HomeWrapper>
         <FeaturedPosts>
           <FeaturedPostList>
-            <FeaturedPost>
-              <Link target='_blank' href='/posts/2023-08-27-3-materiais-para-aprender-testes-automatizados-javascript/'>
-                <FeaturedPostImage>
-                  <Image
-                    src={`/images/home-2023-08-27-3-materiais-para-aprender-testes-automatizados-javascript.png`}
-                    alt="Foto em preto e branco de uma mesa de trabalho com um monitor e computador portátil"
-                    fill
-                    priority
-                  />
-                </FeaturedPostImage>
-                <FeaturedPostHeader>
-                  <FeaturedPostDate>27/08/2023 às 23:00</FeaturedPostDate>
-                  <FeaturedPostTitle>3 materiais para aprender testes automatizados com JavaScript</FeaturedPostTitle>
-                  <FeaturedPostSubTitle>3 recursos (1 livro e 2 cursos) para você aprender e aprofundar seus conhecimentos em testes automatizados com JavaScript</FeaturedPostSubTitle>
-                </FeaturedPostHeader>
-              </Link>
-            </FeaturedPost>
+            <FeaturedPost
+              href='/posts/2023-08-27-3-materiais-para-aprender-testes-automatizados-javascript/'
+              imgSrc='/images/home-2023-08-27-3-materiais-para-aprender-testes-automatizados-javascript.png'
+              imgAlt='Foto em preto e branco de uma mesa de trabalho com um monitor e computador portátil'
+              date='27/08/2023 às 23:00'
+              title='3 materiais para aprender testes automatizados com JavaScript'
+              subtitle='3 recursos (1 livro e 2 cursos) para você aprender e aprofundar seus conhecimentos em testes automatizados com JavaScript'
+            />
 
-            <FeaturedPost>
-              <Link target='_blank' href='https://www.instagram.com/p/CwNfz8vg7oy/'>
-                <FeaturedPostImage>
-                  <Image
-                    src={`/images/5-canais-no-youtube-de-mulheres-programadora.png`}
-                    alt="Foto em preto e branco de um monitor com a logo do Youtube ao centro"
-                    fill
-                    priority
-                  />
-                </FeaturedPostImage>
-                <FeaturedPostHeader>
-                  <FeaturedPostDate>21/08/2023 às 08:00</FeaturedPostDate>
-                  <FeaturedPostTitle>5 canais do Youtube de Mulheres Programadoras</FeaturedPostTitle>
-                  <FeaturedPostSubTitle>Todas elas têm perfis em diversas redes sociais, como Instagram e Linkedin!! Então, aproveita para seguir elas e ficar ligado (a) nos novos conteúdos 🤍</FeaturedPostSubTitle>
-                </FeaturedPostHeader>
-              </Link>
-            </FeaturedPost>
+            <FeaturedPost
+              href='https://www.instagram.com/p/CwNfz8vg7oy/'
+              imgSrc='/images/5-canais-no-youtube-de-mulheres-programadora.png'
+              imgAlt='Foto em preto e branco de um monitor com a logo do Youtube ao centro'
+              date='21/08/2023 às 08:00'
+              title='5 canais do Youtube de Mulheres Programadoras'
+              subtitle='Todas elas têm perfis em diversas redes sociais, como Instagram e Linkedin!! Então, aproveita para seguir elas e ficar ligado (a) nos novos conteúdos 🤍'
+            />
 
-            <FeaturedPost>
-              <Link target='_blank' href='https://www.instagram.com/p/CuR_bC1gBD5/'>
-                <FeaturedPostImage>
-                  <Image
-                    src={`/images/3-livros-que-toda-pessoa-programadora-deveria-ler.png`}
-                    alt="Foto em preto e branco de uma sala grande com várias prateleiras de livros"
-                    fill
-                    priority
-                  />
-                </FeaturedPostImage>
-                <FeaturedPostHeader>
-                  <FeaturedPostDate>04/07/2023 às 07:00</FeaturedPostDate>
-                  <FeaturedPostTitle>3 Livros que uma pessoa programadora deveria ler 📖</FeaturedPostTitle>
-                  <FeaturedPostSubTitle>Livro bom é livro recomendado! Segue a dica com 3 livros que uma pessoa desenvolvedora deveria ler</FeaturedPostSubTitle>
-                </FeaturedPostHeader>
-              </Link>
-            </FeaturedPost>
+            <FeaturedPost
+              href='https://www.instagram.com/p/CuR_bC1gBD5/'
+              imgSrc='/images/3-livros-que-toda-pessoa-programadora-deveria-ler.png'
+              imgAlt='Foto em preto e branco de uma sala grande com várias prateleiras de livros'
+              date='04/07/2023 às 07:00'
+              title='3 Livros que uma pessoa programadora deveria ler 📖'
+              subtitle='Livro bom é livro recomendado! Segue a dica com 3 livros que uma pessoa desenvolvedora deveria ler'
+            />
 
-            <FeaturedPost>
-              <Link target='_blank' href='https://www.instagram.com/p/CuO3tVaALid/'>
-                <FeaturedPostImage>
-                  <Image
-                    src={`/images/5-tecnologias-usadas-no-nubank.png`}
-                    alt="Foto em preto e branco de 3 pessoas olhando para um monitor"
-                    fill
-                    priority
-                  />
-                </FeaturedPostImage>
-                <FeaturedPostHeader>
-                  <FeaturedPostDate>03/07/2023 às 07:00</FeaturedPostDate>
-                  <FeaturedPostTitle>Tecnologias usadas no Nubank</FeaturedPostTitle>
-                  <FeaturedPostSubTitle>Veja as cinco tecnologias mais usadas no Nubank</FeaturedPostSubTitle>
-                </FeaturedPostHeader>
-              </Link>
-            </FeaturedPost>
+            <FeaturedPost
+              href='https://www.instagram.com/p/CuO3tVaALid/'
+              imgSrc='/images/5-tecnologias-usadas-no-nubank.png'
+              imgAlt='Foto em preto e branco de 3 pessoas olhando para um monitor'
+              date='03/07/2023 às 07:00'
+              title='Tecnologias usadas no Nubank'
+              subtitle='Veja as cinco tecnologias mais usadas no Nubank'
+            />
 
-            <FeaturedPost>
-              <Link target='_blank' href='https://www.instagram.com/p/ClJh-Ptr_zw/'>
-                <FeaturedPostImage>
-                  <Image
-                    src={`/images/minha-primeira-palestra-programaria-summit-2022.png`}
-                    alt="Foto em preto e branco que contém a capa parcial de dois livros: a capa do livro da esquerda é sobre refatoração de código e o da direita é a capa do livro Clean Code"
-                    fill
-                    priority
-                  />
-                </FeaturedPostImage>
-                <FeaturedPostHeader>
-                  <FeaturedPostDate>19/11/2022 às 13:00</FeaturedPostDate>
-                  <FeaturedPostTitle>Quando eu realizei a minha primeira palestra</FeaturedPostTitle>
-                  <FeaturedPostSubTitle>Veja como foi minha primeira palestra ténica no Programaria Summit 2022 🤍</FeaturedPostSubTitle>
-                </FeaturedPostHeader>
-              </Link>
-            </FeaturedPost>
+            <FeaturedPost
+              href='https://www.instagram.com/p/ClJh-Ptr_zw/'
+              imgSrc='/images/minha-primeira-palestra-programaria-summit-2022.png'
+              imgAlt='Foto em preto e branco que contém a capa parcial de dois livros: a capa do livro da esquerda é sobre refatoração de código e o da direita é a capa do livro Clean Code'
+              date='19/11/2022 às 13:00'
+              title='Quando eu realizei a minha primeira palestra'
+              subtitle='Veja como foi minha primeira palestra ténica no Programaria Summit 2022 🤍'
+            />
           </FeaturedPostList>
         </FeaturedPosts>
       </HomeWrapper>
---
+++ tests/components/featuredPost.test.js
@@ -0,0 +1,42 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+
+import { FeaturedPost } from '../../src/components/FeaturedPost'
+
+describe('FeaturedPost component', () => {
+  const post = {
+    href: '/posts/2023-08-27-3-materiais-para-aprender-testes-automatizados-javascript',
+    title: '3 materiais para aprender testes automatizados com JavaScript',
+    subtitle: '3 recursos (1 livro e 2 cursos) para você aprender e aprofundar seus conhecimentos em testes automatizados com JavaScript',
+    imgSrc: '/images/home-2023-08-27-3-materiais-para-aprender-testes-automatizados-javascript.png',
+    imgAlt: 'Foto em preto e branco de uma mesa de trabalho com um monitor e computador portátil',
+    date: '27/08/2023 às 23:00'
+  }
+
+  it("should render the post title (h2)", () => {
+    render(<FeaturedPost {...post} />);
+    expect(screen.getByText(post.title)).toBeInTheDocument();
+  })
+
+  it("should render the post sub title (h3)", () => {
+    render(<FeaturedPost {...post} />);
+    expect(screen.getByText(post.subtitle)).toBeInTheDocument();
+  })
+
+  it("should render the post publish date", () => {
+    render(<FeaturedPost {...post} />);
+    expect(screen.getByText(post.date)).toBeInTheDocument();
+  })
+
+  it("should render the post image", () => {
+    render(<FeaturedPost {...post} />);
+    expect(screen.getByAltText(post.imgAlt)).toBeInTheDocument();
+    expect(screen.getByRole('img')).toBeInTheDocument();
+  })
+
+  it("should render the post link", () => {
+    render(<FeaturedPost {...post} />);
+    expect(screen.getByRole('link')).toBeInTheDocument();
+    expect(screen.getByRole('link')).toHaveAttribute('href', post.href);
+  })
+})
